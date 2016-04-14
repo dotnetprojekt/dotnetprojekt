40ed0c38575a7fe4be8b89652e5cf76104857f87
PawelBranka <pawel.branka92@gmail.com>
2016-04-14 18:51:02 +0200

    DEV: Database Clear Script update

--- devSQL/DatabaseClearScript.sql
+++ devSQL/DatabaseClearScript.sql
@@ -1,26 +1,53 @@
 use InvoiceSystem
 
-go
+declare @v_ObjectName nvarchar(4000);
 
-	drop schema "inv";
-
-go
-
-	declare @v_SchemaId int =	(
-								select 
-									schema_id
-								from sys.schemas
-								where name = 'inv'
-							);
+declare @v_SchemaId int =	(
+							select 
+								schema_id
+							from sys.schemas
+							where name = 'inv'
+						);
 	
-
-	select 'drop table "inv.' + name + '";'
+declare cursorTabNames cursor local fast_forward for
+(
+	select 'drop table inv.' + name + ';'
 	from sys.tables
 	where schema_id = @v_SchemaId
+);
+
+open cursorTabNames;
+
+fetch next from cursorTabNames into @v_ObjectName;
+
+while @@FETCH_STATUS = 0
+begin
+	exec sp_executesql @v_ObjectName;
+
+	fetch next from cursorTabNames into @v_ObjectName;
+end
+
+close cursorTabNames;
+deallocate cursorTabNames;
 
 
-	select 'drop procedure "inv.' + name + '";'
+declare cursorProcNames cursor local fast_forward for
+(
+	select 'drop procedure inv.' + name + ';'
 	from sys.procedures
 	where schema_id = @v_SchemaId
+);
 
-	declare cursor 
+open cursorProcNames;
+
+fetch next from cursorProcNames into @v_ObjectName;
+
+while @@FETCH_STATUS = 0
+begin
+	exec sp_executesql @v_ObjectName;
+
+	fetch next from cursorProcNames into @v_ObjectName;
+end
+
+close cursorProcNames;
+deallocate cursorProcNames;
