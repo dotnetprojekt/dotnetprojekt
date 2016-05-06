7637ebe5392f43c235ec89a4ff287dda8ed2f6dd
MD <michald@student.agh.edu.pl>
2016-05-06 15:36:56 +0200

    + identity

---
+++ app/FakturyMVC/FakturyMVC/Controllers/AuthController.cs
@@ -0,0 +1,126 @@
+﻿using FakturyMVC.App_Start;
+using FakturyMVC.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.Owin.Security;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Security.Claims;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FakturyMVC.Controllers
+{
+    [AllowAnonymous]
+    public class AuthController : Controller
+    {
+
+
+        [HttpGet]
+        public ActionResult LogIn(string returnUrl)
+        {
+            var model = new LogInModel
+            {
+                ReturnUrl = returnUrl
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult LogIn(LogInModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+            // Don't do this in production!
+            if (model.Email == "admin@admin.com" && model.Password == "password")
+            {
+                var identity = new ClaimsIdentity(new[] {
+                new Claim(ClaimTypes.Name, "Waldek"),
+                new Claim(ClaimTypes.Email, "a@a.com"),
+                new Claim(ClaimTypes.Country, "Polska")
+            },
+                    "ApplicationCookie");
+
+                var ctx = Request.GetOwinContext();
+                var authManager = ctx.Authentication;
+
+                authManager.SignIn(identity);
+
+                return Redirect(GetRedirectUrl(model.ReturnUrl));
+            }
+
+            // user authN failed
+            ModelState.AddModelError("", "Błędny email lub hasło.");
+            return View();
+        }
+
+        private IAuthenticationManager GetAuthenticationManager()
+        {
+            var ctx = Request.GetOwinContext();
+            return  ctx.Authentication;
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> Register(RegistrationModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View();
+            }
+
+          //  var user = new AppUser
+          //  {
+          //      UserName = model.Email,
+         //       Country = model.Country
+         //   };
+
+            //TODO
+            //var result = await userManager.CreateAsync(user, model.Password);
+
+            //if (result.Succeeded)
+           // {
+           //     await SignIn(user);
+           //     return RedirectToAction("index", "home");
+          //  }
+
+           // foreach (var error in result.Errors)
+           // {
+           //     ModelState.AddModelError("", error);
+           // }
+
+            return View();
+        }
+
+
+
+        public ActionResult LogOut()
+        {
+            var ctx = Request.GetOwinContext();
+            var authManager = ctx.Authentication;
+
+            authManager.SignOut("ApplicationCookie");
+            return RedirectToAction("index", "home");
+        }
+
+        private string GetRedirectUrl(string returnUrl)
+        {
+            if (string.IsNullOrEmpty(returnUrl) || !Url.IsLocalUrl(returnUrl))
+            {
+                return Url.Action("index", "home");
+            }
+
+            return returnUrl;
+        }
+    }
+}
---
+++ app/FakturyMVC/FakturyMVC/Models/AppUser.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace FakturyMVC.Models
+{
+    public class AppUser
+    {
+        public string Email { get; set; }
+
+        public string PasswordHash { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Login { get; set; }
+
+        public bool IsAdmin { get; set; }
+
+    }
+}
